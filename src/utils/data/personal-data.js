132fb8ad2a219aface321e1f76a92d22d93e6251
thealokkushwaha <103347563+thealokkushwaha@users.noreply.github.com>
2024-12-03 00:43:45 +0530

    fiximage

--- src/utils/data/personal-data.js
+++ src/utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Alok Kushwaha",
-  profile: '/Me.webp',
+  profile: './Me.webp',
   designation: "Software Developer",
   description: "Greetings, I'm Alok Kushwaha, a passionate Computer Science and Engineering student specializing in IoT. Proficient in C++, JavaScript, and frameworks like React.js and Node.js, I have developed impactful projects. Additionally, I possess expertise in Unity3D, Augmented Reality (AR), and Virtual Reality (VR). A winner of multiple national hackathons, I excel in blending innovation with technical expertise to solve complex problems. My skills encompass leadership, event management, and teamwork, making me an effective collaborator. I am driven by a commitment to leveraging technology for impactful solutions and continuously strive to enhance my expertise in cutting-edge domains.",
   phone: '+919336103072',
