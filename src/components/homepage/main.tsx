9f20356664afd194d410c29ddd2192d6cec46a90
Alok Kushwaha <103347563+TheAlokkushwaha@users.noreply.github.com>
2024-12-03 13:05:05 +0530

    Update main.tsx

--- src/components/homepage/main.tsx
+++ src/components/homepage/main.tsx
@@ -108,21 +108,19 @@ function Main() {
               <div className="ml-4 lg:ml-8 mr-2">
                 <span className=" text-white">skills:</span>
                 <span className="text-gray-400">{`['`}</span>
-                <span className="text-amber-300">ReactJS</span>
+                <span className="text-amber-300">C</span>
                 <span className="text-gray-400">{"', '"}</span>
-                <span className="text-amber-300">NextJS</span>
+                <span className="text-amber-300">C++</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">ReactJS</span>
                 <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">NodeJS</span>
                 <span className="text-gray-400">{"', '"}</span>
-                <span className="text-amber-300">Express</span>
+                <span className="text-amber-300">Express.js</span>
                 <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">MySql</span>
                 <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">MongoDB</span>
-                <span className="text-gray-400">{"', '"}</span>
-                <span className="text-amber-300">AWS</span>
-                <span className="text-gray-400">{"', '"}</span>
-                <span className="text-amber-300">Python</span>
                 <span className="text-gray-400">{"'],"}</span>
               </div>
               <div>
